c9f6f933bea79113cd03ee7963861bbaf9b26e90
Luis Modesto <luisgmodesto12@gmail.com>
2023-09-27 17:25:09 -0300

    feat: add main component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import styles from './comingPage.module.css'
 import Header  from './header.js'
+import ComingMain  from './comingMain.js'
+
 
 export default function Home() {
   return (
-    <main>
+    <>
         <Header></Header>
-    </main>
+        <ComingMain></ComingMain>
+    </>
   )
 }
